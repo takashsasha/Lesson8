65461f9f914a9bdcd54da20deb46a9a54e280857
RiiFus <di.22rus@yandex.ru>
2022-03-14 21:38:10 +0700

    Задание1

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -10,6 +10,22 @@ namespace Lesson8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"{Properties.Settings.Default.Welcome}");
+            if (Properties.Settings.Default.userName != null)
+            {
+                Console.WriteLine($"\nИмя пользователя:       {Properties.Settings.Default.userName}");
+                Console.WriteLine($"Возраст пользователя:   {Properties.Settings.Default.userAge}");
+                Console.WriteLine($"Профессия пользователя: {Properties.Settings.Default.userProfesson}");
+            }
+
+            Console.WriteLine("\nВведите имя пользователя");
+            Properties.Settings.Default.userName = Console.ReadLine();
+            Console.WriteLine("Введите возраст пользователя");
+            Properties.Settings.Default.userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите профессию пользователя");
+            Properties.Settings.Default.userProfesson = Console.ReadLine();
+            Properties.Settings.Default.Save();
+            Console.ReadKey();
         }
     }
 }
--- Lesson8/Properties/AssemblyInfo.cs
+++ Lesson8/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("Lesson8")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("HellWork Lesson8")]
+[assembly: AssemblyDescription("Урок 8 Задание1")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lesson8")]
+[assembly: AssemblyCompany("Hell")]
+[assembly: AssemblyProduct("HellWork Lesson8")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.6.6")]
+[assembly: AssemblyFileVersion("0.6.6.6")]
